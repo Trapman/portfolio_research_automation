4d0010da1573f503047851fd19ee217c2fba9304
Trapman <david.a.shuttleworth@gmail.com>
2020-07-28 12:05:35 -0400

    Update stock.py

--- stock.py
+++ stock.py
@@ -6,7 +6,12 @@ to be walked through, there are many other data points to explore."""
 
 ############################################
 
-# we will use a free API for personal investing and learning, 'Financial Modeling Prep'
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 11:32:51 2020
+
+@author: das68451
+"""
 
 import requests
 import pandas as pd
@@ -17,3 +22,40 @@ def getdata(stock):
     company_quote = company_quote.json()
     share_price = float("{0:.2f}".format(company_quote[0]['price']))
     
+# Balance Sheet Group of Items    
+    BS = requests.get(f"https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/{stock}?period=quarter")
+    BS = BS.json()
+    
+#Total Debt
+    debt = float("{0:.2f}".format(float(BS['financials'][0]['Total debt'])/10**9))
+#Total Cash
+    cash = float("{0:.2f}".format(float(BS['financials'][0]['Cash and short-term investments'])/10**9))
+    
+# Income Statement Group of Items
+    IS = requests.get(f"https://financialmodelingprep.com/api/v3/financials/income-statement/{stock}?period=quarter")
+    IS = IS.json()
+
+# Most Recent Quarterly Revenue
+    qRev = float("{0:.2f}".format(float(IS['financials'][0]['Revenue'])/10**9))
+    
+# Company Profile Group of Items
+    company_info = requests.get(f"https://financialmodelingprep.com/api/v3/company/profile/{stock}")
+    company_info = company_info.json()
+# Chief Executive Officer
+    ceo = company_info['profile']['ceo']
+    
+    return (share_price, cash, debt, qRev, ceo)
+
+tickers = ('AAPL', 'MSFT', 'GOOG', 'T', 'CSCO', 'INTC', 'ORCL', 'AMZN', 'FB', 'TSLA', 'NVDA')
+    
+data = map(getdata, tickers)
+
+df = pd.DataFrame(data,
+     columns=['Total Cash', 'Total Debt', 'Q3 2019 Revenue', 'CEO'],
+     index=tickers)
+print(df)
+
+# Writing to Excel
+writer = pd.ExcelWriter('example.xlsx')
+df.to_excel(writer, 'Statistics')
+writer.save()
